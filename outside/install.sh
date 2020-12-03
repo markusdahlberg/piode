21f3e245d0abc6f0527c11fc7ab9a508c9d03254
Jim Groth <jim.groth@gmail.com>
2020-12-03 13:43:33 +0100

    Added install scripts

---
+++ outside/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Setup Script for Project Piode Receiver
+
+if [ $(id -u) -eq 0 ]; then
+	cp /src/* /usr/piode/
+	cp /conf/* /etc/piode/
+	cp /systemd/piode-receive.service /etc/systemd/system/
+	systemctl daemon-reload
+	systemctl enable service piode-receive
+	systemctl start service piode-receive
+else
+        echo "Setup failed due to lack of root privileges!"
+        exit 1
+fi
